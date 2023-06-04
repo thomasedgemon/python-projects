b628602a61835adb62d182ae2ddcdabe8f35d4fa
Thomas Edgemon <44883486+thomasedgemon@users.noreply.github.com>
2023-06-04 16:10:30 -0500

    Delete very simple PRNG.py

--- very simple PRNG.py
+++
@@ -1,9 +0,0 @@
-import numpy 
-import time 
-import datetime
-import math
-
-ordered_pair = math.modf((time.time()))
-base = ordered_pair[0] + ordered_pair[1]
-result = (base ** 5) % 100
-print(result)
